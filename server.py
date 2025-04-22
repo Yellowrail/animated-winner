a34c1d31ce3696b53f8bd5f5703211c97bd2b3c1
Yellowrail <yellowrail.na@gmail.com>
2025-04-22 04:21:36 -0400

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,12 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import torch.nn.functional as F
 from flask_cors import CORS
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host='0.0.0.0', port=port)
+
 
 app = Flask(__name__)
 CORS(app)  # Allow CORS for local testing
